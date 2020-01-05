12059d346be8dfd5733479edee8544c2c3cdf2f6
matildahmariwa <matildamariwa3@gmail.com>
2020-01-05 22:34:41 +0300

    webpack and vue cli

---
+++ src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="box">
+<p>
+  <slot>
+
+  </slot>
+</p>
+</div>
+</template>
+<script>
+
+</script>
+<style>
+  .box{
+    background: #e3e3e3;
+    padding: 18px;
+    border:1px solid grey;
+  }
+</style>
